619c8aa554a70a5a338140c2bd607eb348272d3c
Justin <justincyork@gmail.com>
2012-08-13 14:24:33 -0600

    Update fs records widget
    Create a more robust parsing engine for the FamilySearch records page

--- widgets/fs-record-search.js
+++ widgets/fs-record-search.js
@@ -1,6 +1,8 @@
-(function(rs){
+(function(rs, undefined){
 
   rs.registerWidget(['familysearch.org'], verifySetup);
+  
+  var hasFamilyTable = $('.result-data .household-label').length > 0;
   
   function verifySetup() {
     if( window.location.pathname.indexOf('/pal:/') == 0 ) {
@@ -20,49 +22,54 @@
     var flyout = $('<nav class="flyout share-flyout"><div class="flyout-direction"><div></div></div></nav>').appendTo(searchTrigger);
     var linksList = $('<ul>').prependTo(flyout);
     
-    var nameParts = rs.splitName( $.trim( $('.result-data tr:first td[itemprop="name"]').text() ) );
+    // Process the table rows of the record data
+    // The first cell in each row becomes the key
+    // The second cell becomes a list of the cells in that row
+    // We store all the cells because the gender of
+    // family members on census records is important
+    var recordData = {};
+    $('.result-data tr').each(function(){
+      var row = $(this);
+      var fieldName = $.trim( $('td:first', row).text().toLowerCase().slice(0, -1) );
+      if( fieldName ) {
+        recordData[fieldName] = $('td', row);
+      }
+    });
+    
+    var nameParts = rs.splitName( getCleanCellValue( recordData['name'], 1 ) );
     
     var personData = {
       'givenName': nameParts[0],
       'familyName': nameParts[1],
-      'birthDate': $.trim( $('#birth_date').text() ),
-      'birthPlace': $.trim( $('#birth_location td[itemprop="name"]').text() ),
-      'deathDate': $.trim( $('#death_date').text() ),
-      'deathPlace': $.trim( $('#death_location td[itemprop="name"]').text() )
+      'birthDate': checkMultipleFields( recordData, ['birth date', 'birthdate', 'estimated birth year', 'estimated birth date'] ),
+      'birthPlace': checkMultipleFields( recordData, ['birthplace', 'place of birth'] ),
+      'deathDate': getCleanCellValue( recordData['death date'], 1 ),
+      'deathPlace': getCleanCellValue( recordData['death place'], 1 )
     };
-        
-    /**
-        {
-          'givenName': givenName,
-          'familyName': familyName,
-          'birthPlace': birthPlace,
-          'birthDate': birthDate,
-          'deathPlace': deathPlace,
-          'deathDate': deathDate,
-          'fatherGivenName': fatherGivenName,
-          'fatherFamilyName': fatherFamilyName,
-          'motherGivenName': motherGivenName,
-          'motherFamilyName': motherFamilyName,
-          'spouseGivenName': spouseGivenName,
-          'spouseFamilyName': spouseFamilyName
-        }
-     */
-     
-    /**
-     * Now that we have data in the correct format, we pass it to the
-     * rs.executeLinkBuilders() function. This will pass the data on
-     * to all of the search link builders and return an array of link
-     * objects in the following format:
-     *
-     *  { 'text': 'My Example Link', 'url': 'http://myexamplesearchdomain.com/searchstuff' }
-     *
-     * Asking the link builders to only return the link text and url
-     * allows for the widgets to build the links however they want:
-     * as <a> or <button> tags, with classes and styles that match
-     * the page, etc.
-     *
-     * Loop over the link array and add your links to the widget.
-     */
+    
+    // Look for a spouse
+    var spouseName = getSpousesName(recordData);
+    if( spouseName ) {
+      var spouseNameParts = rs.splitName( spouseName );
+      personData['spouseGivenName'] = spouseNameParts[0];
+      personData['spouseFamilyName'] = spouseNameParts[1];
+    }
+    
+    // Look for a mother
+    var motherName = getParentName(recordData, 'mother');
+    if( motherName ) {
+      var motherNameParts = rs.splitName( motherName );
+      personData['motherGivenName'] = motherNameParts[0];
+      personData['motherFamilyName'] = motherNameParts[1];
+    }
+
+    // Look for a father
+    var fatherName = getParentName(recordData, 'father');
+    if( fatherName ) {
+      var fatherNameParts = rs.splitName( fatherName );
+      personData['fatherGivenName'] = fatherNameParts[0];
+      personData['fatherFamilyName'] = motherNameParts[1];
+    }
     
     var linkData = rs.executeLinkBuilders(personData);
      
@@ -70,6 +77,87 @@
       linksList.append( '<li><a href="'+link.url+'" target="_blank">'+link.text.toUpperCase()+'</a></li>' );
     });
 
+  }
+  
+  // Check for the existence of multiple fields
+  // First one found is returned
+  function checkMultipleFields( recordData, fields ) {
+    for( var i in fields ) {
+      if( recordData[fields[i]] ) {
+        return getCleanCellValue( recordData[fields[i]], 1 );
+      }
+    }
+    return undefined;
+  }
+  
+  function getCleanCellValue( cells, position ) {
+    if( cells ) {
+      return $.trim( cells.eq(position).text() );
+    }
+    return undefined;
+  }
+  
+  function getRelationship(recordData) {
+    return checkMultipleFields( recordData, ["relationship to head of household", "relationship to head of household (standardized)"]).toLowerCase();
+  }
+  
+  function getSpousesName(recordData) {
+    // Check to see if the "spouse's name" is set
+    if( recordData["spouse's name"] ) {
+      return getCleanCellValue( recordData["spouse's name"], 1 );
+    }
+    
+    // If "spouse's name" isn't set do some crazy relationship jiu-jitsu
+    else if( hasFamilyTable ) {
+      var relationship = getRelationship(recordData);
+      
+      // The husband is always listed as the head of household
+      // so we only look for the wife. If the wife is the head
+      // of household it means the husband isn't there so returning
+      // the wife will be undefined which means there is no spouse
+      if( relationship == "head" ) {
+        return getCleanCellValue( recordData['wife'], 1);
+      } else if( relationship == "wife" ) {
+        return getCleanCellValue( recordData['head'], 1);
+      }
+    }
+    
+    return undefined;
+  }
+
+  function getParentName(recordData, parent) {
+    // Check to see if the "parent's name" is set
+    if( recordData[parent+"'s name"] ) {
+      return getCleanCellValue( recordData[parent+"'s name"], 1);
+    }
+    
+    // If "parent's name" isn't set do some crazy relationship jiu-jitsu
+    else if( hasFamilyTable ) {
+      var relationship = getRelationship(recordData);
+      
+      if( relationship == 'son' || relationship == 'daughter' ) {
+        var headGender = getCleanCellValue( recordData['head'], 2);
+        
+        if( parent == 'father' ) {
+          
+          // Check to see if the gender of the head of household is male
+          if( headGender == 'M' ) {
+            return getCleanCellValue( recordData['head'], 1);
+          }
+        } else if( parent == 'mother' ) {
+          
+          // If the head of household is male, return the wife's name
+          // If the head of household is female, return the head's name
+          if( headGender == 'F' ) {
+            return getCleanCellValue( recordData['head'], 1);
+          } else if( headGender == 'M' ) {
+            return getCleanCellValue( recordData['wife'], 1);
+          }
+        }
+      }
+    }
+    
+    return undefined;
   }
 
 }(rs));
