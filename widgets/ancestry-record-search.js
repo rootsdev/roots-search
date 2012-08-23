9acd69f284267e414e9c93741a9220b29063243c
Justin <justincyork@gmail.com>
2012-08-22 22:01:54 -0600

    Add basic parsing to ancestry record widget
    #9

--- widgets/ancestry-record-search.js
+++ widgets/ancestry-record-search.js
@@ -4,7 +4,10 @@
    * Setup the widget when the page loads.
    */
   $(document).ready(function(){
-    setup();
+    if( $('#record-header').length == 1) {
+      console.log("we're going in");
+      setup();
+    }
   });
   
   function setup() {
@@ -32,75 +35,55 @@
     var widget = $('<div>')
       .addClass('recordHints')
       .append('<div class="recordHintsHd"><h3>Roots Search</h3></div>')
-      .appendTo(widgetWrap);
-    
+      .appendTo(widgetWrap);   
     var linksWrap = $('<div class="recordHintsBd">').appendTo(widget);
     
-    linksWrap.append( $('<a class="rs-search-link" href="#">Example</a>') );
-    
     /**
-     * Next we retrieve the data on the screen. How this is done depends
-     * on the site. For FamilySearch it was easiest to make AJAX
-     * calls to the apis which the site uses. However, most sites will
-     * involve screen scraping.
-     */
-    var firstName = $('#first-name');
-    var lastName = $('#last-name');
-    
-    /**
-     * After retrieving information from the site, it needs to be converted into 
-     * the format that the search link builders will expect. They expect an object
-     * in following format:
-     
-        {
-          'givenName': givenName,
-          'familyName': familyName,
-          'birthPlace': birthPlace,
-          'birthDate': birthDate,
-          'deathPlace': deathPlace,
-          'deathDate': deathDate,
-          'fatherGivenName': fatherGivenName,
-          'fatherFamilyName': fatherFamilyName,
-          'motherGivenName': motherGivenName,
-          'motherFamilyName': motherFamilyName,
-          'spouseGivenName': spouseGivenName,
-          'spouseFamilyName': spouseFamilyName
-        }
-     
-     * None of the attributes are required. Visit the Wiki or examine other
-     * widgets for more information on the data format.
-     */
-     
-    var personData = {
-      'givenName': firstName,
-      'familyName': lastName
-    };
-     
-    /**
-     * Now that we have data in the correct format, we pass it to the
-     * rs.executeLinkBuilders() function. This will pass the data on
-     * to all of the search link builders and return an array of link
-     * objects in the following format:
-     *
-     *  { 'text': 'My Example Link', 'url': 'http://myexamplesearchdomain.com/searchstuff' }
-     *
-     * Asking the link builders to only return the link text and url
-     * allows for the widgets to build the links however they want:
-     * as <a> or <button> tags, with classes and styles that match
-     * the page, etc.
-     *
-     * Loop over the link array and add your links to the widget.
+     * Next we begin retrieving the data on the screen. First we
+     * process the result table.
      */
     
-    return;
+    var personData = {};
+    var recordData = {};
+    $('.p_resultTable tr').each(function(){
+      var row = $(this);
+      // Take the row label, trim leading and trailing whitespace, 
+      // lowercase it, and remove the trailing ":".
+      // This will serve as the key in the recordData object
+      var label = $.trim( $('th', row).text() ).toLowerCase().slice(0, -1);
+      if( label && !recordData[label] ) {
+        recordData[label] = row;
+      }
+    });
+    
+    // Process the name
+    if( recordData.name ) {
+      var nameParts = rs.splitName( $.trim( recordData.name.children().eq(1).text() ) );
+      personData.givenName = nameParts[0];
+      personData.familyName = nameParts[1];
+    }
+    
+    // Process estimated birth year
+    if( recordData['estimated birth year'] ) {
+      personData.birthDate = $.trim( recordData['estimated birth year'].children().eq(1).text() ).substr(4);
+    }
+    
+    // Process the birthplace
+    if( recordData.birthplace ) {
+      personData.birthPlace = $.trim( recordData.birthplace.children().eq(1).text() );
+    }
+    
+    /**
+     * Build the links
+     */
     
     var linkData = rs.executeLinkBuilders(personData);
      
     $.each(linkData, function(i, link) {
-      $('<a>').addClass('search-links').attr({
+      $('<a>').addClass('rs-search-links').attr({
         'target': '_blank',
         'href': link.url
-      }).html(link.text).appendTo(widget);
+      }).html(link.text).appendTo( $('<div>').css('margin', '8px 0px').appendTo(linksWrap) );
     });
      
     /**
