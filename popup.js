1766b5d2d5e0f556b702af0196bb02190b057e89
Justin <justincyork@gmail.com>
2012-09-18 21:40:45 -0600

    First round of styling

--- popup.js
+++ popup.js
@@ -1,21 +1,23 @@
+var bgPage;
+
 $(document).ready(function(){
   
   // Get the id of the tab
   chrome.tabs.query({active: true}, function(tabs){
     
-    var bgPage = chrome.extension.getBackgroundPage();
+    bgPage = chrome.extension.getBackgroundPage();
     
     var personData = bgPage.personDataObjects[tabs[0].id];
     
     fillForm(personData);
     
-    var searchLinks = bgPage.rs.executeLinkBuilders(personData);
+    updateLinks();
     
-    $.each(searchLinks, function(i, link){
-      $('<div>')
-        .appendTo('#search-links')
-        .append( $('<a target="_blank">').html(link.text).attr('href', link.url) );
-    });
+  });
+  
+  // Bind the click event to the update button
+  $('#update-button').click(function(){
+    updateLinks();
   });
   
 });
@@ -35,4 +37,37 @@ function fillForm(personData) {
   $('#father-last-name').val(personData.fatherFamilyName);
   $('#mother-first-name').val(personData.motherGivenName);
   $('#mother-last-name').val(personData.motherFamilyName);
+}
+
+function updateLinks() {
+  
+  // Delete previous links
+  $('#search-links').html('');
+  
+  var personData = {
+    givenName: $('#first-name').val(),
+    familyName: $('#last-name').val(),
+    birthDate: $('#birth-date').val(),
+    birthPlace: $('#birth-place').val(),
+    deathDate: $('#death-date').val(),
+    deathPlace: $('#death-place').val(),
+    spouseGivenName: $('#spouse-first-name').val(),
+    spouseFamilyName: $('#spouse-last-name').val(),
+    marriageDate: $('#marriage-date').val(),
+    marriagePlace: $('#marriage-place').val(),
+    fatherGivenName: $('#father-first-name').val(),
+    fatherFamilyName: $('#father-last-name').val(),
+    motherGivenName: $('#mother-first-name').val(),
+    motherFamilyName: $('#mother-last-name').val()
+  };
+  
+  var searchLinks = bgPage.rs.executeLinkBuilders(personData);
+  
+  $.each(searchLinks, function(i, link){
+    $('<div>')
+      .addClass('link-wrap')
+      .appendTo('#search-links')
+      .append( $('<a target="_blank">').html(link.text).attr('href', link.url) );
+  });
+  
 }
