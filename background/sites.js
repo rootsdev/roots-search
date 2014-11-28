75f05598dcf77c81a09e195c7d5afc783b24f792
Justin <justincyork@gmail.com>
2014-11-28 10:53:35 -0600

    Better options page

--- background/sites.js
+++ background/sites.js
@@ -3,76 +3,91 @@ var sites = [
   {
     key: 'ancestry',
     name: 'Ancestry.com',
+    url: 'http://www.ancestry.com',
     enabled: true
   },
   {
     key: 'archives',
     name: 'Archives',
+    url: 'http://www.archives.com',
     enabled: true
   },
   {
     key: 'billiongraves',
     name: 'BillionGraves',
+    url: 'http://billiongraves.com',
     enabled: true
   },
   {
     key: 'familysearch',
     name: 'FamilySearch',
+    url: 'https://familysearch.org',
     enabled: true
   },
   {
     key: 'findagrave',
     name: 'Find A Grave',
+    url: 'http://findagrave.com',
     enabled: true
   },
   {
     key: 'findmypast',
     name: 'Findmypast',
+    url: 'http://www.findmypast.co.uk',
     enabled: true,
   },
   {
     key: 'fold3',
     name: 'Fold3',
+    url: 'http://www.fold3.com',
     enabled: false
   },
   {
     key: 'genealogieonline',
     name: 'Genealogie Online',
+    url: 'http://www.genealogieonline.nl/en/',
     enabled: false
   },
   {
     key: 'genealogybank',
     name: 'Genealogy Bank',
+    url: 'http://www.genealogybank.com/gbnk/',
     enabled: false
   },
   {
     key: 'geni',
     name: 'Geni',
+    url: 'http://www.geni.com',
     enabled: false
   },
   {
     key: 'myheritage',
     name: 'MyHeritage',
+    url: 'http://www.myheritage.com',
     enabled: true
   },
   {
     key: 'newspapers',
     name: 'Newspapers',
+    url: 'http://www.newspapers.com',
     enabled: false
   },
   {
     key: 'openarchives',
     name: 'Open Archives',
+    url: 'https://www.openarch.nl',
     enabled: false
   },
   {
     key: 'werelate',
     name: 'WeRelate.org',
+    url: 'http://www.werelate.org',
     enabled: false
   },
   {
     key: 'worldvitalrecords',
     name: 'World Vital Records',
+    url: 'http://www.worldvitalrecords.com',
     enabled: false
   }
 ];
--- options/options.js
+++ options/options.js
@@ -5,18 +5,28 @@ $(document).ready(function(){
   $sites = $('#sites').html('');
   
   $.each(background.sites, function(i, site){
-    $sites.append(creatSiteDOM(site));
+    $sites.append(
+      $('<tr>').append(
+        $('<td>').append(creatSiteDOM(site))
+      )
+    );
   });
 
 });
 
 function creatSiteDOM(site){
-  var dom = $('<div>').addClass('site checkbox'),
+  var dom = $('<div>').addClass('site checkbox').addClass(site.enabled ? '' : 'rs-disabled'),
       label = $('<label>').text(site.name).appendTo(dom),
-      input = $('<input type="checkbox">').prependTo(label).attr('checked', site.enabled);
+      input = $('<input type="checkbox">').prependTo(label).attr('checked', site.enabled),
+      url = $('<div class="site-url"><a target="_blank" href="' + site.url + '">' + site.url + '</a></div>').appendTo(dom);
   input.change(function(){
     site.enabled = $(this).is(':checked');
     background.saveSiteSettings(site);
+    if(site.enabled){
+      dom.removeClass('rs-disabled');
+    } else {
+      dom.addClass('rs-disabled');
+    }
   });
   return dom;
 };
