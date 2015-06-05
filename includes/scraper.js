234f16e3ec66b5b53fba3ed3ce247b4fc6973abf
Justin <justincyork@gmail.com>
2015-06-05 11:37:32 -0500

    Remove console logs

--- includes/scraper.js
+++ includes/scraper.js
@@ -5,21 +5,22 @@ chrome.extension.sendRequest({
 
 genscrape()
   .on('data', function(data){
-    console.log('data');
     chrome.extension.sendRequest({
       'type': 'person_info',
       'data': data
     });
   })
   .on('noData', function(){
-    console.log('no data');
     chrome.extension.sendRequest({
       'type': 'hide'
     });
   })
+  /*
+  // TODO: logging
   .on('noMatch', function(){
-    console.log('no match');
+    
   })
+  */
   .on('error', function(e){
     chrome.extension.sendRequest({
       'type': 'js_error',
